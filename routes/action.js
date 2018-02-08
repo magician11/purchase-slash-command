809bdb330e135222680d08fc3888ba78f73c42c7
Andrew Golightly <support@andrewgolightly.com>
2018-02-09 08:50:09 +1300

    clean up and reading Firebase node

--- routes/action.js
+++ routes/action.js
@@ -4,7 +4,6 @@ const { readPurchaseRequest } = require('../modules/database');
 module.exports = app => {
   app.post('/action', async (req, res) => {
     const interactiveMessage = JSON.parse(req.body.payload);
-    console.log(interactiveMessage);
     const requestApproved = interactiveMessage.actions[0].value === 'approved';
     const databaseKey = interactiveMessage.actions[0].name;
     const originalTextMessage = interactiveMessage.original_message.text;
@@ -23,15 +22,11 @@ module.exports = app => {
 
     // read purchase request from database using key
     const purchaseRequest = await readPurchaseRequest(databaseKey);
-    console.log('purchaseRequest from Firebase...');
-    console.log(purchaseRequest);
 
     // send the original purchase requester feedback on the decision
-    const matches = originalTextMessage.match(/@(.+)>.+\*(.+)\*/);
-    console.log(matches);
     sendDM(
-      matches[1],
-      `Hi! Your purchase request for ${matches[2]} was ${
+      purchaseRequest.userId,
+      `Hi! Your purchase request for ${purchaseRequest.item} was ${
         requestApproved ? 'approved' : 'denied'
       }.`
     );
--- routes/purchase.js
+++ routes/purchase.js
@@ -11,7 +11,6 @@ module.exports = app => {
 
     // save purchase request to database
     const key = savePurchaseRequest(user_id, text);
-    console.log(key);
 
     // send a message to the CEO.. asking them to authorise the purchase request
     sendDM(config.ceoId, `Hi! <@${user_id}> would like to order *${text}*.`, [
